edcf423f04ce35357b9d0ea2f62d11d7f87d2cc5
greg <g.kanavenko@gmail.com>
2024-03-31 18:48:25 +0300

    added search for entry

--- reports/brokerage_monthly.py
+++ reports/brokerage_monthly.py
@@ -1,7 +1,7 @@
 from openpyxl import load_workbook
 from pathlib import Path
 
-MAX_EXCEL_ROWS_NUM = 2000
+MAX_EXCEL_ROWS_NUM = 20000
 EXCEL_START_COLUMN = 2
 
 END_OF_REPORT_STR = 'Владелец: ООО "Компания БКС"'
@@ -15,6 +15,7 @@ class Securities:
         self.securities: list[dict] | None = None
         self.sheet = self.workbook[self.workbook.sheetnames[0]]
         self.securities_entry_pos: int | None = self.find_securities_entry()
+        self.load()
 
     def load(self):
         pass
@@ -22,8 +23,11 @@ class Securities:
     def find_securities_entry(self) -> int | None:
         for i in range(1, MAX_EXCEL_ROWS_NUM):
             cell = self.sheet.cell(row=i, column=self.start_column)
-            if cell.value == SECURITIES_ENTRY_STR:
-                print(i)
+            cell_next = self.sheet.cell(row=i + 1, column=self.start_column)
+            if cell.value == SECURITIES_ENTRY_STR and cell_next.value == 'Вид актива':
+                entry_index = i + 2
+                cell = self.sheet.cell(row=entry_index, column=self.start_column)
+                print(entry_index)
                 print(cell.row, cell.column)
                 print(cell.coordinate)
                 return cell
